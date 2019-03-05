7a97f81cfbc004e2ea0ec911712023a8d334df3a
Heather Peri <heatherhperi@gmail.com>
2019-03-04 23:33:21 -0500

    updated js

--- assets/Javascript/Crystals.js
+++ assets/Javascript/Crystals.js
@@ -1,12 +1,12 @@
 
-var randomresult;
+var randomresult = 0;
 var win = 0;
-var lose = 0;
-var first = 0;
+var lost = 0;
+var previous = 0;
 
-var ResetstartGame = function(){
+// var ResetstartGame = function(){
 
-  $(".crystals").empty();
+//   $(".crystals").empty();
   
 randomresult = Math.floor(Math.random() * 90 ) + 30;
 
@@ -17,50 +17,57 @@ for(var i = 0; i < 4; i++){
   var random = Math.floor(Math.random() * 18 ) + 1;
 
   var crystal = $("<div>");
-    crystal.attr({
-      "class": 'crystal',
-      "data-random": random
-    });
+      crystal.attr({
+        "class": 'crystal',
+        "data-random": random
+      });
 
-    crystal.html(random);
+      crystal.html(random);
 
-    // $(".crystals").append(crystal);
+    $(".crystals").append(crystal);
 
-  }
-      }
+}
 
-      ResetstartGame();
-  $(document).on('click', ".crystal", function (){
-
-   
+//       ResetstartGame();
+  $(".crystal").on('click', function () { 
 
     var num = parseInt($(this).attr('data-random'));
 
-    first += num;
+    previous += num;
 
-    if(first > randomresult){
+    if(previous > randomresult){
       lost++;
-      
-      first = 0;
-
-      $("$win").html(lost);
-      ResetstartGame();
+      $("$lost").html(lost);
     }
-
-    else(first === randomresult) {
+    
+    else(previous === randomresult){
       win++;
-
       $("$win").html(win);
-
-      first = o;
-      ResetstartGame();
-
     }
-
   });
 
+//     if(first > randomresult){
+//       lost++;
+      
+//       first = 0;
 
-{/* <button id="Crystal1"  value="1"><span>1</span></button>
+//       $("$win").html(lost);
+//       ResetstartGame();
+//     }
+
+//     else(first === randomresult) {
+//       win++;
+
+
+//       first = 0;
+//       ResetstartGame();
+
+//     }
+
+//   });
+
+
+/* <button id="Crystal1"  value="1"><span>1</span></button>
 <button id="Crystal2"  value="2"><span>2</span></button>
 <button id="Crystal3"  value="3"><span>3</span></button>
 <button id="Crystal4"  value="4"><span>4</span></button>
@@ -103,4 +110,4 @@ for (var i = 19; i < 120; i++) {
 
   var randomnum = Math.floor(Math.random() * 120);
 
-} */}
+} */
