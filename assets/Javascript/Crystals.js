dc70645b8174278efbb6d27b042cd4b0fbd050cd
Heather Peri <heatherhperi@gmail.com>
2019-03-05 22:06:21 -0500

    added css

--- assets/Javascript/Crystals.js
+++ assets/Javascript/Crystals.js
@@ -4,45 +4,67 @@ var win = 0;
 var lost = 0;
 var previous = 0;
 
-// var ResetstartGame = function(){
+var ResetstartGame = function(){
 
-//   $(".crystals").empty();
+  $(".crystals").empty();
   
-randomresult = Math.floor(Math.random() * 90 ) + 30;
+  var images = ["https://i.pinimg.com/236x/1d/e6/92/1de692ac8dc11b976acd924c1dd90a3d--crystal-drawing-google-search.jpg", 
+  "https://i.pinimg.com/originals/5e/4a/6f/5e4a6ff52e7a5e2e4923d4dbeebe9a4a.jpg", 
+  "https://i.pinimg.com/736x/fb/f3/33/fbf3333d4a0d906fe34fa0c46994c235--crystal-drawing-crystal-tattoo.jpg", 
+  "https://designformankind.com/images/2010/08/gem-drawing-412x563.jpg"];
 
-$("#result").html('Random Number: ' + randomresult);
+  randomresult = Math.floor(Math.random() * 90 ) + 30;
 
-for(var i = 0; i < 4; i++){
+  $("#result").html('Random Number: ' + randomresult);
 
-  var random = Math.floor(Math.random() * 18 ) + 1;
+  for(var i = 0; i < 4; i++){
 
-  var crystal = $("<div>");
-      crystal.attr({
-        "class": 'crystal',
-        "data-random": random
-      });
+    var random = Math.floor(Math.random() * 18 ) + 1;
 
-      crystal.html(random);
+    var crystal = $("<div>");
+        crystal.attr({
+          "class": 'crystal',
+          "data-random": random
+        });
 
-    $(".crystals").append(crystal);
+        crystal.css({
+          "background-image":"url('" + images[i] + ""')",
+          "background-size": "cover"
+        });
 
-}
+      $(".crystals").append(crystal);
 
-//       ResetstartGame();
-  $(".crystal").on('click', function () { 
+  }
+
+$("#previous").html("Total Score: " + previous);
+
+      ResetstartGame();
+
+      var reset = function () {
+
+      }
+  $(document).on('click', ".crystal", function () { 
 
     var num = parseInt($(this).attr('data-random'));
 
     previous += num;
 
+    $("#previous").html("Total Score: " + previous);
+
     if(previous > randomresult){
       lost++;
-      $("$lost").html(lost);
+      $("$lost").html("lost!" + lost);
+      previous = 0;
+      
+      ResetstartGame();
     }
     
     else(previous === randomresult){
       win++;
-      $("$win").html(win);
+      $("$win").html("win!" + win);
+      previous = 0;
+      
+      ResetstartGame();
     }
   });
 
@@ -60,54 +82,4 @@ for(var i = 0; i < 4; i++){
 
 
 //       first = 0;
-//       ResetstartGame();
 
-//     }
-
-//   });
-
-
-/* <button id="Crystal1"  value="1"><span>1</span></button>
-<button id="Crystal2"  value="2"><span>2</span></button>
-<button id="Crystal3"  value="3"><span>3</span></button>
-<button id="Crystal4"  value="4"><span>4</span></button>
-
-
-$(document).ready(function() {
-    
-    var Cry1 = 1-19;
-    var Cry2 = 1-19;
-    var Cry3 = 1-19;
-    var Cry4 = 1-19;
-
-$("Cry1").on("click", function() {
-
-  for (var i = 19; i < 120; i++) {
-
-  var randomnum = Math.floor(Math.random() * 120);
-
-      }
-      
-$("Cry2").on("click", function()
-
-for (var i = 19; i < 120; i++) {
-
-  var randomnum = Math.floor(Math.random() * 120);
-
-}
-
-$("Cry3").on("click", function()
-
-for (var i = 19; i < 120; i++) {
-
-  var randomnum = Math.floor(Math.random() * 120);
-
-}
-
-$("Cry4").on("click", function()
-
-for (var i = 19; i < 120; i++) {
-
-  var randomnum = Math.floor(Math.random() * 120);
-
-} */
